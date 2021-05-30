d1631597dc7568b81c4c203655cad8db87924ffc
Gray Lewis <grayjordann@gmail.com>
2021-05-30 20:16:43 +0200

    Added error handling uploadFile

--- uploadFile.py
+++ uploadFile.py
@@ -30,16 +30,22 @@ for current_argument, current_value in arguments:
 try:
   file_name
 except:
-  file_name = "gtp3Config.json"
-  print("Defaulting to gpt3Config.json. To provide a different config file to be uploaded, please use the -f=FILE_NAME argument.")
+  file_name = "gpt3Config.json"
+  print("Defaulting to gpt3Config.json. To provide a different config file to be uploaded, please use the -f FILE_NAME argument.")
 # END COMMAND LINE PARSING
 
 openai.api_key = api_key
-file = openai.File.create(
-  file=open(file_name),
-  purpose='classifications'
-)
-
-print(file)
-print("IMPORTANT: THE FOLLOWING ID NEEDS TO BE SAVED FOR REFERENCE.")
-print("The ID of the file that was uploaded is %s" % (file["id"]))
+try:
+  file = openai.File.create(
+    file=open(file_name),
+    purpose='classifications'
+  )
+except IOError as e:
+  print("There was no file found at %s" % file_name)
+except Exception as e:
+  print("There was an error while uploading the file: ")
+  print(e)
+else:
+  print(file)
+  print("IMPORTANT: THE FOLLOWING ID NEEDS TO BE SAVED FOR REFERENCE.")
+  print("The ID of the file that was uploaded is %s" % (file["id"]))
